4ade933f103be40de4a362427283421c764a74ec
Emil Sundström <emil.sundstrom@control.lth.se>
2023-11-10 14:41:35 +0100

    Adding a dummy main method of the PID implementation

---
+++ main.m
@@ -0,0 +1,44 @@
+% Setup (filters, states, etc.)
+
+%%%%%%%%%%%%% Start
+
+% Measurement signal input: y
+% Reference signal: r
+
+% Filtering
+
+
+% Controller state (MAN, AUTO)
+
+
+% Compute P, I, D
+P = 1;
+I = 2;
+D = 3;
+
+% Compensate time-jittering
+
+
+% Bumpless flag
+
+
+% Clamping and anti-windup for max/min
+
+
+% Clamping and anti-windup for max derivative
+
+
+% Update I
+
+
+% Set control signal
+PID = P + I + D;
+
+disp(PID);
+
+
+
+
+
+
+
